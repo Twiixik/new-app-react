706b4d25fa4a310d69ac4eed8e2ad3b8794e097f
Twiixik <119367167+Twiixik@users.noreply.github.com>
2024-10-22 10:37:30 +0200

    Updated HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,22 @@
+import { useEffect, useState } from 'react';
+
 export default function HomePage() {
+    const [posts, setPosts] = useState([]); // Correctly destructuring useState with square brackets
+
+    useEffect(() => {
+        async function fetchPosts() {
+            const url = "https://timotejsproject-default-rtdb.europe-west1.firebasedatabase.app/posts.json";
+            const response = await fetch(url);
+            const data = await response.json();
+            console.log(data);
+            const postsArray = Object.keys(data).map(key => ({ id: key, ...data[key] })); // from object to array
+            setPosts(postsArray);
+        }
+        
+        fetchPosts(); // Call the function correctly
+        
+    }, []); // Ensure the dependency array is closed properly
+
     return (
         <section className="page">
             <h1>Home Page</h1>
